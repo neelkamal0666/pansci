f37f1899506a4087e2aa070c5ca4b9fef044cf99
neelkamal0666 <neelkamal0666@gmail.com>
2014-03-25 19:08:17 +0530

    js file url updated

--- resources/layout/admin/footer.php
+++ resources/layout/admin/footer.php
@@ -1,10 +1,10 @@
   <!-- JavaScript -->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="resources/js/bootstrap.js"></script>
+    <script src="<?php echo SITE_URL; ?>/resources/js/bootstrap.js"></script>
 
     <!-- Page Specific Plugins -->
       
-	 <script src="resources/js/custom_js.js"></script>
+	 <script src="<?php echo SITE_URL; ?>/resources/js/custom_js.js"></script>
 
   </body>
 </html>
