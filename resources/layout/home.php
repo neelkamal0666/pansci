abb04b7a2cf1eda44789574453d95c69981e0578
neelkamal0666 <neelkamal0666@gmail.com>
2014-03-27 15:46:01 +0530

    list of colleges added

--- resources/layout/home.php
+++ resources/layout/home.php
@@ -29,31 +29,63 @@
             </div>
             	 	           	      
    		</div>
-   		<div class="main">
-		   <div class="wrap">
-			 <div class="content-top">
-				<div class="col_1_of_3 span_1_of_3">
-					<h3>EDC</h3>
-					<img src="<?php echo SITE_URL; ?>/resources/images/icon.png" alt=""/>
-					<p>Entrepreneurship development program for students initiated by MSME.EDP is a 6/15 days Entrepreneurship Development e-learning module through CD and other sources.</p>
-					<div class="btn-link"><a href="<?php echo SITE_URL;?>/services">read more</a></div>
-				</div>
-				<div class="col_1_of_3 span_1_of_3">
-					<h3>NIESBUD</h3>
-					<img src="http://niesbudtraining.org/images/HeaderNew.png" alt=""/>
-					<p>NIESBUD is an Apex Institute in the area of Entrepreneurship and Small Business Development under the Ministry of Micro, Small and Medium Enterprises , Government of India. </p>
-					<div class="btn-link"><a href="http://niesbudtraining.org" target="_blank">read more</a></div>
-				</div>
-				<div class="col_1_of_3 span_1_of_3">
-					<h3>MSME</h3>
-					<img src="http://msme.gov.in/emblem.jpg" height="100" alt=""/>
-					<p>Worldwide, the micro small and medium enterprises (MSMEs) have been accepted as the engine of economic growth and for promoting equitable development. The major advantage of the sector is its employment potential at low capital cost. .</p>
-					<div class="btn-link"><a href="http://msme.gov.in" target="_blank">read more</a></div>
-				</div>
-			    <div class="clear"></div>
-			</div>
-		   </div>
+   		 <div class="clients">
+	  <div class="wrap">
+	  	<h2 class="head">Colleges</h2>
+		 <ul id="flexiselDemo3">
+			<li><b>NIT Raipur</b></li>
+			<li><b>BIT Mesra</b></li>
+			<li><b>IIIT Bangalore</b></li>
+			<li><b>NIT Durgapur</b></li>
+		 </ul>
+	<script type="text/javascript">
+		$(window).load(function() {
+			$("#flexiselDemo1").flexisel();
+			$("#flexiselDemo2").flexisel({
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
 		
-	</div>
+			$("#flexiselDemo3").flexisel({
+				visibleItems: 5,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: true,
+				enableResponsiveBreakpoints: true,
+		    	responsiveBreakpoints: { 
+		    		portrait: { 
+		    			changePoint:480,
+		    			visibleItems: 1
+		    		}, 
+		    		landscape: { 
+		    			changePoint:640,
+		    			visibleItems: 2
+		    		},
+		    		tablet: { 
+		    			changePoint:768,
+		    			visibleItems: 3
+		    		}
+		    	}
+		    });
+		    
+		});
+	</script>
+	<script type="text/javascript" src="<?php echo SITE_URL; ?>/resources/scripts/jquery.flexisel.js"></script>
+     </div>
+	</div>	  
     
    </div>  <!-- Site footer -->
